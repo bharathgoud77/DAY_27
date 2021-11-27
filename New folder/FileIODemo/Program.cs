1af6e3d7ca2754011218257565367450fcb38fa6
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-11-27 15:07:10 +0530

    executed

---
+++ New folder/FileIODemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\fellowship\DAY_15\TXt.txt";
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            else
+            {
+                Console.WriteLine("File NOt Exists");
+            }
+        }
+        static void ReadAlllines(string path)
+        {
+
+            string[] lines;
+            lines = File.ReadAllLines(path);
+
+            //Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+        }
+            static void ReadAllText(string path)
+            {
+                string lines;
+                lines = File.ReadAllText(path); ;
+                Console.WriteLine(lines);
+            }
+        
+    }
+}
+    
